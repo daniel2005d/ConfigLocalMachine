150ae09819b244528721397a27199d8789317ae5
daniel <daniel@localhost.localdomain>
2023-10-10 14:24:24 -0500

    Fix Errors

--- ffuf.sh
+++ ffuf.sh
@@ -6,7 +6,7 @@ if [ ! -d /opt/ffuf ]; then
     git clone https://github.com/ffuf/ffuf
     cd /opt/ffuf;go get;go build
     cd -
-    if [ ! -f /usr/bin/ffuf ] then;
+    if [ ! -f /usr/bin/ffuf ]; then
         sudo ln -s /opt/ffuf/ffuf /usr/bin/ffuf  
     fi
 fi
