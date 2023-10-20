ce17bf18009f53ee6808cfc9e7289f01c5fae5e1
Daniel <daniel2005d@gmail.com>
2023-10-20 09:47:13 -0500

    Fix Error

--- sqlmap.sh
+++ sqlmap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -d /opt/sqlmap ]; then
-    git clone https://github.com/sqlmapproject/sqlmap.git -o /opt/sqlmap
+    cd /opt; git clone https://github.com/sqlmapproject/sqlmap.git 
     if [ ! -f /usr/bin/sqlmap ]; then
         sudo ln -s /opt/sqlmap/sqlmap /usr/bin/sqlmap  
     fi
