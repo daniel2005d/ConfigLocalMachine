bc39ebca52cc13a730a257b116860010733c057a
daniel2005d <daniel2005d@gmail.com>
2024-03-08 09:37:05 -0500

    Update sqlmap symlink

--- sqlmap.sh
+++ sqlmap.sh
@@ -3,7 +3,7 @@
 if [ ! -d /opt/sqlmap ]; then
     cd /opt; git clone https://github.com/sqlmapproject/sqlmap.git 
     if [ ! -f /usr/bin/sqlmap ]; then
-        sudo ln -s /opt/sqlmap/sqlmap /usr/bin/sqlmap  
+        sudo ln -s /opt/sqlmap/sqlmap.py /usr/bin/sqlmap  
     fi
     
 fi
