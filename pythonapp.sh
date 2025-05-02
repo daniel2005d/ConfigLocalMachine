9d314e0c177c75e83cb9b4202fdb2ce1786d728f
Your Name <you@example.com>
2025-05-02 10:49:25 -0400

    Fix pythonapp installer.

--- pythonapp.sh
+++ pythonapp.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 
+if ! command -v run-httpserver > /dev/null; then
+    print_info "Installing HttpServer"
+    cd /opt;git clone https://github.com/daniel2005d/HttpServer.git
+    cd /opt/HttpServer/Server
+    pip install -e . --break-system-packages
+fi
 
-print_info "Installing HttpServer"
-cd /opt;git clone https://github.com/daniel2005d/HttpServer.git
-cd /opt/HttpServer/Server
-pip install -e . --break-system-packages
-
-print_info "Installing scripts"
-cd /opt;git clone https://github.com/daniel2005d/scripts.git
-cd /opt/scripts
-pip install -e . --break-system-packages.
+if ! command -v run-metadata > /dev/null; then
+    print_info "Installing scripts"
+    cd /opt;git clone https://github.com/daniel2005d/scripts.git
+    cd /opt/scripts
+    pip install -e . --break-system-packages
+fi
