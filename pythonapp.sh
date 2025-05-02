28a524cc4aea34e849b00e5152089ea0a1cf38c8
daniel2005d <daniel2005d@gmail.com>
2025-05-02 09:29:30 -0500

    Fix HttpServer Installer

--- pythonapp.sh
+++ pythonapp.sh
@@ -4,8 +4,8 @@
 
 print_info "Installing HttpServer"
 cd /opt;git clone https://github.com/daniel2005d/HttpServer.git
-cd /opt/HttpServer
-pip install -e . --break-system-packages.
+cd /opt/HttpServer/Server
+pip install -e . --break-system-packages
 
 print_info "Installing scripts"
 cd /opt;git clone https://github.com/daniel2005d/scripts.git
