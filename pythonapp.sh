518eec299aa16e842aa3341dc84f4f7453e2bb91
daniel2005d <daniel2005d@gmail.com>
2025-05-02 09:25:50 -0500

    Add BSP at pip install command

--- pythonapp.sh
+++ pythonapp.sh
@@ -5,9 +5,9 @@
 print_info "Installing HttpServer"
 cd /opt;git clone git@github.com:daniel2005d/HttpServer.git
 cd /opt/HttpServer
-pip install -e .
+pip install -e . --break-system-packages.
 
 print_info "Installing scripts"
 cd /opt;git clone https://github.com/daniel2005d/scripts.git
 cd /opt/scripts
-pip install -e .
+pip install -e . --break-system-packages.
