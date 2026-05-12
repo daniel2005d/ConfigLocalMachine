10e959db0c636c0559f5908cd6a2769b18fdd945
daniel2005d <daniel2005d@gmail.com>
2026-05-12 11:30:07 -0500

    Ajuste de instalación google chrome

--- software.sh
+++ software.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -o /tmp/google-chrome-stable_current_amd64.deb
-sudo apt install /tmp/google-chrome-stable_current_amd64.deb
+# 1. Add the signing key
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo tee /etc/apt/trusted.gpg.d/google.asc >/dev/null
+
+# 2. Add the repository to your system
+echo "deb http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/chrome.list
+
+# 3. Update package list and install Chrome
+sudo apt update
+sudo apt install google-chrome-stable
